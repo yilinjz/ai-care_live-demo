3c7c7f18cc0ad3535dd1b46d7dc9f67d7b806906
Jason Zhang <71229867+yilinjz@users.noreply.github.com>
2024-07-15 16:58:00 +0800

    checkpoint

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 import gradio as gr
 
+from const import DEMO_IMG_DICT
+
+
 def create_interface():
     '''
-    define the user interface with gradio
+    define the user interface
     '''        
     with gr.Blocks() as user_interface:
-      with gr.Row():
-        with gr.Column():
-          im = gr.Image()
-          dd = gr.Dropdown(["far", "medium", "close"], label="select")
-        with gr.Column():
-          il = gr.Image()
-        with gr.Column():
-          cb = gr.Chatbot()
-          voice = gr.Audio(sources=["microphone"])
-    
+      # Tab 1: demo in Cantonese
+      # Fixed scene images/sample questions/audio inputs
+      with gr.Tab("Cantonese Demo"):
+        # Layout: (1) Raw Scene Image (2) Scene Image processed by Yolo (3) Chatbot 
+        with gr.Row():
+          # select demo image
+          with gr.Column():
+            # default image
+            demo_img = gr.Image("images\demo_scene_view_1.jpg")
+            demo_img_dropdown = gr.Dropdown(DEMO_IMG_DICT.keys(), label="select view")
+          with gr.Column():
+            demo_yolo_result = gr.Image()
+          with gr.Column():
+            chatbot = gr.Chatbot()
+            voice_input = gr.Audio(sources=["microphone"])
 
+      with gr.Tab("Interactive English Demo"):
+         pass
+    
     return user_interface
+
 
 if __name__ == "__main__":
     '''
---
+++ const.py
@@ -0,0 +1,6 @@
+# Demo Image 
+DEMO_IMG_DICT = {
+    "view_1": "images\demo_scene_view_1.jpg",
+    "view_2": "images\demo_scene_view_2.jpg",
+    "view_3": "images\demo_scene_view_3.jpg",
+}
